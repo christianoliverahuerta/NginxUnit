679548d1584c96916f4d946b913cf7290e0e3060
Christian Olivera Huerta <christian.olivera@gmail.com>
2024-04-28 06:31:50 +0000

    secuencia de comandos

--- js/script.js
+++ js/script.js
@@ -9,6 +9,17 @@ toggle.addEventListener("click", () =>{
 	sidebar.classList.toggle("close");
 });
 
+searchBtn.addEventListener("click", () =>{
+	sidebar.classList.remove("close");
+});
+
 modeSwitch.addEventListener("click", () =>{
 	body.classList.toggle("dark");
+
+	if(body.classList.contains("dark")){
+		modeText.innerText = "Light Mode"
+	}else{
+		modeText.innertext = "Dark Mode" 
+	}
+
 });
